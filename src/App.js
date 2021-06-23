c6856c67fec70f4a8fd463c41277671a9ab5c53a
Aniketh <aniketh12@gmail.com>
2021-06-23 13:46:30 +0530

    Contact, submit, map completed

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import About from './components/About';
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 import Testimonials from "./components/Testimonials";
+import Contact from './components/Contact';
 
 export default function App() {
   return (
@@ -11,6 +12,7 @@ export default function App() {
       <Projects />
       <Skills />
       <Testimonials />
+      <Contact />
     </main>
   );
 }
