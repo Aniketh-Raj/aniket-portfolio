401cf1a9e50db17bff4920fa748e1167ed4bf9a6
Aniketh <aniketh12@gmail.com>
2021-06-23 13:31:43 +0530

    Testimonials added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import About from './components/About';
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
+import Testimonials from "./components/Testimonials";
 
 export default function App() {
   return (
@@ -9,6 +10,7 @@ export default function App() {
       <About />
       <Projects />
       <Skills />
+      <Testimonials />
     </main>
   );
 }
